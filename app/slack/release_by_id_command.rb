f2edc6a370aba7e26608df74c64421ecd58c184d
Jakub Polak <jakub.polak.vz@gmail.com>
2022-02-06 20:35:27 +0100

    changes

---
+++ app/slack/release_by_id_command.rb
@@ -0,0 +1,29 @@
+class ReleaseByIdCommand < Slackathon::Command
+  def call
+    # Check if spot is available
+    spot = Spot.where(id: params[:text])
+    if spot.status == "reserved"
+      {
+        response_type: "in_channel",
+        text: "The spot with number #{spot.id} is not available at the moment. Try to book a different spot."
+      }
+    else
+      available_spot = spot
+      # make a new booking
+      booking = Booking.new(
+        pickup_time: Time.now,
+        return_time: Time.now + 1.day,
+        spot_id: available_spot.id,
+        # user_id: params[:user_id].to_i,
+        user_id: 3
+      )
+      available_spot.status = "reserved"
+      available_spot.save!
+      booking.save!
+      {
+        response_type: "in_channel",
+        text: "Booked parking spot with number #{booking.spot_id}."
+      }
+    end
+  end
+end
