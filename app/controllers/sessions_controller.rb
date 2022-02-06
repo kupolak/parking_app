449a3e947507238a8f02a1d5c6edf1dfd018cbbf
Jakub Polak <jakub.polak.vz@gmail.com>
2022-02-06 20:16:56 +0100

    Changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,15 @@
 class SessionsController < ApplicationController
   def create
-    # @user = User.find_or_create_by(full_name: auth_hash.info.name, member_id: auth_hash.info.user_id)
-    #
+    # @user = User.find_or_create_by(member_id: request.env['omniauth.strategy'].access_token.to_hash["user_id"])
     # session[:user_id] = @user.id
     # redirect_to '/'
-    # render plain: { access_token: request.env['omniauth.strategy'].access_token.to_hash,
-    #                 auth_hash:  request.env['omniauth.auth']
-    # }.to_yaml
-  end
-
-  protected
-  def auth_hash
-    request.env['omniauth.auth']
+    render plain: { access_token: request.env['omniauth.strategy'].access_token.to_hash,
+                    auth_hash:  request.env['omniauth.auth']
+    }.to_yaml
+    # p "testy"
+    # binding.pry
+    # p request.env['omniauth.strategy']
+    # p "dupa"
+    # p request.env['omniauth.auth']
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_131354) do
+ActiveRecord::Schema.define(version: 2022_02_06_185407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2022_02_05_131354) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "full_name"
     t.string "member_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
